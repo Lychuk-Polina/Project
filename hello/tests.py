41a02971579141fafe233d64311bdf52d7d4a06c
Lychuk-Polina <lychukpolina@gmail.com>
2021-05-23 20:23:11 +0300

    first commit

---
+++ hello/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from django.test import SimpleTestCase
+from django.test import Client
+
+class HomePageViewTestCase(SimpleTestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_request_home_page(self):
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, 'Hello world!',count=None, status_code=200, msg_prefix='', html=False)
